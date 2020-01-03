4abd641da1e0b73af1290812bb059d8c2a7680e7
yugank-drx <yugank@datarainx.com>
2020-01-03 20:35:18 +0530

    updated the changes

---
+++ server.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const app = express();
+router = express.Router();
+
+app.use(function (req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Methods", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, token");
+    next();
+});
+
+
+app.use(express.static(__dirname + '/dist/'));
+
+router.get('/*', function (req, res) {
+
+    res.sendFile(path.join(__dirname + '/index.html'));
+
+  });
+
+var port =  process.env.PORT || 8081;
+
+app.listen(port, function () {
+    console.log('Express server listening on port ' + port + ' in ' + app.get('env') + ' mode');
+});
